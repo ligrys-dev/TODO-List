f963b6b594c9d6659781bb863d9f2a8c133012b0
Maciej Ligęza <maclig92@gmail.com>
2023-02-04 14:23:18 +0100

    refactoring removeTask with data atribute

--- delete.js
+++ delete.js
@@ -1,7 +1,12 @@
+// const removeTask = e => {
+//   // e.target.parentNode.remove();
+//   e.target.parentNode.style.textDecoration = 'line-through';
+//   e.target.remove();
+// };
+
 const removeTask = e => {
-  // e.target.parentNode.remove();
-  e.target.parentNode.style.textDecoration = 'line-through';
-  e.target.remove();
+  const index = e.target.dataset.key;
+  document.querySelector(`li[data-key="${index}"]`).remove();
 };
 
 document.querySelectorAll('li button').forEach(btn => {
