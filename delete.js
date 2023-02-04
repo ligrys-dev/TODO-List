e7c72eaf157421b4f1d63fd394db33792d7f7e52
Maciej Ligęza <maclig92@gmail.com>
2023-02-04 14:16:51 +0100

    change to strikethrough and delete btn

--- delete.js
+++ delete.js
@@ -1,5 +1,7 @@
 const removeTask = e => {
-  e.target.parentNode.remove();
+  // e.target.parentNode.remove();
+  e.target.parentNode.style.textDecoration = 'line-through';
+  e.target.remove();
 };
 
 document.querySelectorAll('li button').forEach(btn => {
